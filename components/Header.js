2807d8f0bd34fafe17836a9f90b0c3353250f9cc
Ilija <nukawanderer@gmail.com>
2021-11-20 17:10:51 +0800

    built the header

--- components/Header.js
+++ components/Header.js
@@ -1,10 +1,20 @@
 import HeaderItem from "./HeaderItem";
-import { HomeIcon } from "@heroicons/react/outline";
+import {
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
   return (
-    <header className="">
-      <HeaderItem title="HOME" Icon={HomeIcon} />
+    <header className="m-5">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
     </header>
   );
 }
