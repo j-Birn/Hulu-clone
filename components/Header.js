c72c5cec92ee89231164e0c880d512f02922f6d5
Ilija <nukawanderer@gmail.com>
2021-12-02 16:44:22 +0800

    added  navbar

--- components/Header.js
+++ components/Header.js
@@ -9,7 +9,7 @@ import {
 function Header() {
   return (
     <header className="m-5">
-      <div className="flex flex-grow justify-evenly max-w-2xl">
+      <div className="flex flex-grow justify-evenly ">
         <HeaderItem title="HOME" Icon={HomeIcon} />
         <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
